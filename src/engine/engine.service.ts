a9cf691e78903f7d740a3554214d7ceb0a68b304
revolage_a_kob <revolage.a.kob@gmail.com>
2024-02-12 15:03:29 +0600

    engineService static difficulty levels added, analysis listening reworked, refactoring

--- src/engine/engine.service.ts
+++ src/engine/engine.service.ts
@@ -1,31 +1,63 @@
 import { Injectable } from '@nestjs/common';
-import { StockfishInstance } from 'node-stockfish';
+import { StockfishInstance, StockfishAnalysis } from 'node-stockfish';
+
+type SkillLevel = {
+  level: string;
+  depth: number;
+};
 
 @Injectable()
 export class EngineService {
- 
-  private stockfish: StockfishInstance;
-  constructor() {
-    this.stockfish = StockfishInstance.getInstance()
-  }
+  private readonly stockfishWhite: StockfishInstance;
+  private readonly stockfishBlack: StockfishInstance;
+  private readonly skillLevels: SkillLevel[] = [
+    { level: 'easy', depth: 1 },
+    { level: 'medium', depth: 5 },
+    { level: 'hard', depth: 20 }
+  ];
+  private readonly playerLevels: { w: SkillLevel; b: SkillLevel };
 
-  async getBestMove(fen: string, depth: number = 5): Promise<string> {
+  constructor() {
+    this.stockfishWhite = StockfishInstance.getInstance();
+    this.stockfishBlack = StockfishInstance.getInstance();
+    // Randomly assign difficulty levels for white and black
+    this.playerLevels = {
+      w: this.skillLevels[Math.floor(Math.random() * this.skillLevels.length)],
+      b: this.skillLevels[Math.floor(Math.random() * this.skillLevels.length)]
+    };
+    // Info message with timeout to appear after project initialization
+    setTimeout(() => {
+      console.log(`White will play at level "${this.playerLevels.w.level}" with depth ${this.playerLevels.w.depth}, ` +
+        `Black at level "${this.playerLevels.b.level}" with depth ${this.playerLevels.b.depth}.`);
+    },500)
+  }
+  async getBestMove(fen: string) {
     return new Promise((resolve, reject) => {
-      this.stockfish.setBoardstateByFen(fen);
-      this.stockfish.startAnalysing({ lines: 1 });
+      const player = fen.toString().split(' ')[1];
+      const stockfish =
+        player === 'w' ? this.stockfishWhite : this.stockfishBlack;
+      stockfish.setBoardstateByFen(fen);
+      stockfish.startAnalysing({ lines: 1 });
 
       const onAnalysis = (analysis: { lines: string | any[] }) => {
-        if (analysis.lines.length > 0) {
-          const bestMove = analysis.lines[0].moves[0];
-          console.log('resolved line: ', analysis)
-          console.log('resolved score: ', analysis.lines[0].score)
-          resolve(bestMove);
-          this.stockfish.stopAnalysing();
-          this.stockfish.onAnalysisData(null); // Remove listener to prevent memory leaks
+        if (analysis['noLegalMoves'] === true) {
+          console.log('No legal moves available - game ended.');
+          reject('noLegalMoves');
+        } else if (
+          analysis.lines.length > 0 &&
+          stockfish.currentDepth === this.playerLevels[player].depth - 1
+        ) {
+          console.log('analysis: ', analysis);
+          console.log('analysis lines: ');
+          console.log(analysis.lines[0]);
+          resolve(analysis.lines[0].moves[0]);
+
+          // Cleanup listener to avoid memory leak
+          stockfish.stopAnalysing();
+          stockfish.onAnalysisData(null);
         }
       };
-
-      this.stockfish.onAnalysisData(onAnalysis);
+      stockfish.onAnalysisData(onAnalysis);
     });
   }
 }
